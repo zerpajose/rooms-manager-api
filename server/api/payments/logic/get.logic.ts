8162de09f347a0f70197ecbc7c9a79c98cc6128c
Jose Zerpa <zjose88@gmail.com>
2023-12-24 20:34:38 -0300

    🐛 Fix bug in getPayment function

--- server/api/payments/logic/get.logic.ts
+++ server/api/payments/logic/get.logic.ts
@@ -2,7 +2,7 @@ import Payment from '../../../models/Payment.model';
 
 export async function getPayment(paymentId: string) {
 
-  const paymentIntent = await Payment.findOne({ paymentId });
+  const paymentIntent = await Payment.findOne({ paymentId, deletedAt: { $exists: false } });
 
   return paymentIntent?.toObject();
 }
