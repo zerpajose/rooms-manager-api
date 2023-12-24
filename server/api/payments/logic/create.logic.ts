972c7d36f517cb6f6e82148ef6065f6046484355
Jose Zerpa <zjose88@gmail.com>
2023-12-24 19:00:46 -0300

    👔 Add paymentMethod parameter to createPayment function

--- server/api/payments/logic/create.logic.ts
+++ server/api/payments/logic/create.logic.ts
@@ -2,15 +2,14 @@ import { v4 as uuidv4 } from 'uuid';
 import Payment from '../../../models/Payment.model';
 
 export async function createPayment(payment: any) {
-  const { amount, currency, description } = payment;
+  const { amount, currency, description, paymentMethod } = payment;
 
   const paymentIntent = await Payment.create({
     paymentId: uuidv4(),
     amount,
     currency,
     description,
-    status: 'pending',
-    paymentMethod: 'cash',
+    paymentMethod,
     createdAt: new Date(),
     updatedAt: new Date(),
   });
